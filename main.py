80238af98d8097cf6e80d472a766d6f60b29b673
Jess Barnes <jessicabarnes@ufl.edu>
2023-10-25 15:38:19 -0400

    Lab 6 Part 1 changes

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+password=input("what is your password?")
+finpassword=list(password)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+for i in range(0,8):
+    finpassword[i]=int(finpassword[i])+3
+    finpassword[i]=str(finpassword[i])
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+''.join(finpassword)
+print(finpassword)
