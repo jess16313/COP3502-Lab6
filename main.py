3908753cf12b340371965c7728d24c5323b27242
Jess Barnes <jessicabarnes@ufl.edu>
2023-10-25 15:50:44 -0400

    Lab 6 Part 2 changes

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-password=input("what is your password?")
-finpassword=list(password)
+def coding():
+    password=input("what is your password?")
+    finpassword=list(password)
 
-for i in range(0,8):
-    finpassword[i]=int(finpassword[i])+3
-    finpassword[i]=str(finpassword[i])
+    for i in range(0,8):
+        finpassword[i]=int(finpassword[i])+3
+        finpassword[i]=str(finpassword[i])
 
-finpassword=''.join(finpassword)
-print(finpassword)
+    finpassword=''.join(finpassword)
+    print(finpassword)
+
+
+if __name__ == "__main__":
+    coding()
